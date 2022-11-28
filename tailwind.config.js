0e6f9c2790cf3d4c31f9b16267ff6be314e0fc79
hellen0506 <hellen@dtn.com.vn>
2022-11-28 15:45:48 +0700

    Update clipboard website

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,20 @@ module.exports = {
   theme: {
     fontFamily: {
       barlow: ['Barlow Condensed', 'sans-serif'],
-      baijam: ['Bai Jamjuree', 'sans-serif']
+      baijam: ['Bai Jamjuree', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif'],
     },
     extend: {
       backgroundImage: {
         'pattern-quote': "url('./../images/testimonial/bg-pattern-quotation.svg')",
         'clipboard-header-dt': "url('./../images/clipboard/bg-header-desktop.png')",
         'clipboard-header-mb': "url('./../images/clipboard/bg-header-mobile.png')",
+      },
+      maxWidth: {
+        'custom': '1220px',
+      },
+      colors: {
+        'purple-light': '#829b9a',
       }
     },
   },
