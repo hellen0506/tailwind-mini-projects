9cb4f8804c2dd3557c32fbe08f2d7aebda52f43d
hellen0506 <hellen@dtn.com.vn>
2022-11-30 11:10:51 +0700

    Update shortly website

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,11 @@ module.exports = {
         'cyan-btn':"#2ACFCF",
       },
       backgroundImage: {
-        'pattern-quote': "url('./../images/testimonial/bg-pattern-quotation.svg')",
-        'clipboard-header-dt': "url('./../images/clipboard/bg-header-desktop.png')",
-        'clipboard-header-mb': "url('./../images/clipboard/bg-header-mobile.png')",
-        'boost-dt': "url('./../images/shortly/bg-boost-desktop.svg')",
-        'boost-mb': "url('./../images/shortly/bg-boost-mobile.svg')",
+        'pattern-quote': "url('./../assets/images/testimonial/bg-pattern-quotation.svg')",
+        'clipboard-header-dt': "url('./../assets/images/clipboard/bg-header-desktop.png')",
+        'clipboard-header-mb': "url('./../assets/images/clipboard/bg-header-mobile.png')",
+        'boost-dt': "url('./../assets/images/shortly/bg-boost-desktop.svg')",
+        'boost-mb': "url('./../assets/images/shortly/bg-boost-mobile.svg')",
       },
       maxWidth: {
         'custom': '1220px',
