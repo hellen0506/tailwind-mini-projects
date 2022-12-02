54df73bc23fbca1a8221ff6ca3cf59030fdfb136
hellen0506 <hellen@dtn.com.vn>
2022-12-02 10:49:22 +0700

    Update shortly website

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,31 @@
 console.log('Application running.');
+
+const formShortLink = document.querySelector('#form-short-link');
+const inputURL = document.querySelector('#input-url');
+const errorMess = document.querySelector('#error-valid');
+if (formShortLink) {
+    formShortLink.addEventListener('submit', handleShortLink);
+}
+function handleShortLink(e) {
+    e.preventDefault();
+    console.log(inputURL.value, 'dd');
+    if (inputURL.value == '') {
+        errorMess.innerText = 'Please enter shorten link';
+        errorMess.classList.add('block');
+        errorMess.classList.remove('hidden');
+    }
+    console.log(value);
+}
+function isValidHttpUrl(string) {
+    console.log(string);
+    let url;
+    try {
+        url = new URL(string);
+        errorMess.classList.add('hidden');
+    } catch (_) {
+        errorMess.innerText = 'Please enter a valid URL';
+        errorMess.classList.add('block');
+        errorMess.classList.remove('hidden');
+    }
+    return url.protocol === 'http:' || url.protocol === 'https:';
+}
